a92b9d7cab037a9bdaf170cf894726fda355b0be
mmzhirkov <mmzhirkov@yandex.ru>
2020-06-01 02:59:22 +0300

    Add project files.

---
+++ FlushMarketDataBinance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlushMarketDataBinance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
